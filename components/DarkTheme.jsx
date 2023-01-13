c16bc4aa9487b4d7b121efbba84c7882600311d2
meykiacs <meykiacs@gmail.com>
2023-01-13 22:57:07 +0330

    dark theme implemented

---
+++ components/DarkTheme.jsx
@@ -0,0 +1,13 @@
+export default function DarkTheme() {
+  return (
+    <style jsx global>
+      {`
+        :root {
+          --background-color: rgb(26, 25, 25);
+          --link-color: rgb(221, 199, 4);
+          --text-color: rgb(228, 228, 228);
+        }
+      `}
+    </style>
+  )
+}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,37 +1,38 @@
-import Link from "next/link";
-import ThemeSwitch from "./ThemeSwitch";
+import Link from 'next/link'
+import ThemeSwitch from './ThemeSwitch'
 
 export default function Navbar() {
   return (
     <nav>
       <ul>
         <li>
-          <Link href='/'>Home</Link>
+          <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href='/about'>About</Link>
+          <Link href="/about">About</Link>
         </li>
       </ul>
       <ThemeSwitch />
 
-      <style jsx>{`
-        nav {
-          display: flex;
-          justify-content: space-between;
-        }
-        ul {
-          list-style-type: none;
-          padding: 0;
-        }
-        
-        li {
-          display: inline;
-        }
-        
-        li:not(:first-child) {
-          margin-left: 0.75rem;
-        }        
-      `}
+      <style jsx>
+        {`
+          nav {
+            display: flex;
+            justify-content: space-between;
+          }
+          ul {
+            list-style-type: none;
+            padding: 0;
+          }
+
+          li {
+            display: inline;
+          }
+
+          li:not(:first-child) {
+            margin-left: 0.75rem;
+          }
+        `}
       </style>
     </nav>
   )
