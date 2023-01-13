1b6d71be31461a352001e58e3b757906f84a65a0
meykiacs <meykiacs@gmail.com>
2023-01-14 03:11:58 +0330

    dark theme stored

--- components/ThemeSwitch.jsx
+++ components/ThemeSwitch.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import DarkTheme from './DarkTheme'
 
 export default function ThemeSwitch() {
   console.log('theme switch component called')
   const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    const value = localStorage.getItem('darkMode')
+    setDarkMode(value === null ? false : JSON.parse(value))
+  }, [])
+
   const text = darkMode ? 'Light Mode' : 'Dark Mode'
+
+  const handleClick = e => {
+    setDarkMode(!darkMode)
+    // no need to check for localStorage availability
+    // since the handleClick only happens in the browser
+    localStorage.setItem('darkMode', JSON.stringify(!darkMode))
+  }
 
   return (
     <>
-      <button onClick={() => setDarkMode(!darkMode)}>{text}</button>
+      <button onClick={handleClick}>{text}</button>
       <style jsx>
         {`
           button {
